aa7326a0947a45ebdfb99e029455be68cb4da7f8
Michael Ørregaard Andersen <michael@initiva.se>
2023-10-10 13:50:24 +0200

    Editing some test files to ejs and updated routing

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const port = 2500;
-const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const serveStatic = require('serve-static');
-const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/Images'));
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render("3000-5000/Image-1/image-test-avif");
-});
+// 3000-5000 tests
+app.get("/3000-5000/image-1/avif", (req, res) => res.render("3000-5000/Image-1/image-test-avif"));
+app.get("/3000-5000/image-1/webp", (req, res) => res.render("3000-5000/Image-1/image-test-webp"));
+app.get("/3000-5000/image-1/gif", (req, res) => res.render("3000-5000/Image-1/image-test-gif"));
+app.get("/3000-5000/image-1/png", (req, res) => res.render("3000-5000/Image-1/image-test-png"));
+app.get("/3000-5000/image-1/jpg", (req, res) => res.render("3000-5000/Image-1/image-test-jpg"));
+
+app.get("/3000-5000/image-2/avif", (req, res) => res.render("3000-5000/Image-2/image-test-avif"));
+app.get("/3000-5000/image-2/webp", (req, res) => res.render("3000-5000/Image-2/image-test-webp"));
+app.get("/3000-5000/image-2/gif", (req, res) => res.render("3000-5000/Image-2/image-test-gif"));
+app.get("/3000-5000/image-2/png", (req, res) => res.render("3000-5000/Image-2/image-test-png"));
+app.get("/3000-5000/image-2/jpg", (req, res) => res.render("3000-5000/Image-2/image-test-jpg"));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
