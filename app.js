fe826e9f13b5448047ef985b1e9915800f943260
wherecani <michael_andersen93@pm.me>
2023-11-26 21:45:06 +0100

    Adding the routes for test in 500-1000 size

--- app.js
+++ app.js
@@ -186,6 +186,48 @@ app.get("/500-1000/image-2/gif", (req, res) => res.render("500-1000/Image-2/imag
 app.get("/500-1000/image-2/png", (req, res) => res.render("500-1000/Image-2/image-test-png"));
 app.get("/500-1000/image-2/jpg", (req, res) => res.render("500-1000/Image-2/image-test-jpg"));
 
+app.get("/500-1000/image-3/avif", (req, res) => res.render("500-1000/Image-3/image-test-avif"));
+app.get("/500-1000/image-3/webp", (req, res) => res.render("500-1000/Image-3/image-test-webp"));
+app.get("/500-1000/image-3/gif", (req, res) => res.render("500-1000/Image-3/image-test-gif"));
+app.get("/500-1000/image-3/png", (req, res) => res.render("500-1000/Image-3/image-test-png"));
+app.get("/500-1000/image-3/jpg", (req, res) => res.render("500-1000/Image-3/image-test-jpg"));
+
+app.get("/500-1000/image-4/avif", (req, res) => res.render("500-1000/Image-4/image-test-avif"));
+app.get("/500-1000/image-4/webp", (req, res) => res.render("500-1000/Image-4/image-test-webp"));
+app.get("/500-1000/image-4/gif", (req, res) => res.render("500-1000/Image-4/image-test-gif"));
+app.get("/500-1000/image-4/png", (req, res) => res.render("500-1000/Image-4/image-test-png"));
+app.get("/500-1000/image-4/jpg", (req, res) => res.render("500-1000/Image-4/image-test-jpg"));
+
+app.get("/500-1000/image-5/avif", (req, res) => res.render("500-1000/Image-5/image-test-avif"));
+app.get("/500-1000/image-5/webp", (req, res) => res.render("500-1000/Image-5/image-test-webp"));
+app.get("/500-1000/image-5/gif", (req, res) => res.render("500-1000/Image-5/image-test-gif"));
+app.get("/500-1000/image-5/png", (req, res) => res.render("500-1000/Image-5/image-test-png"));
+app.get("/500-1000/image-5/jpg", (req, res) => res.render("500-1000/Image-5/image-test-jpg"));
+
+app.get("/500-1000/image-6/avif", (req, res) => res.render("500-1000/Image-6/image-test-avif"));
+app.get("/500-1000/image-6/webp", (req, res) => res.render("500-1000/Image-6/image-test-webp"));
+app.get("/500-1000/image-6/gif", (req, res) => res.render("500-1000/Image-6/image-test-gif"));
+app.get("/500-1000/image-6/png", (req, res) => res.render("500-1000/Image-6/image-test-png"));
+app.get("/500-1000/image-6/jpg", (req, res) => res.render("500-1000/Image-6/image-test-jpg"));
+
+app.get("/500-1000/image-7/avif", (req, res) => res.render("500-1000/Image-7/image-test-avif"));
+app.get("/500-1000/image-7/webp", (req, res) => res.render("500-1000/Image-7/image-test-webp"));
+app.get("/500-1000/image-7/gif", (req, res) => res.render("500-1000/Image-7/image-test-gif"));
+app.get("/500-1000/image-7/png", (req, res) => res.render("500-1000/Image-7/image-test-png"));
+app.get("/500-1000/image-7/jpg", (req, res) => res.render("500-1000/Image-7/image-test-jpg"));
+
+app.get("/500-1000/image-8/avif", (req, res) => res.render("500-1000/Image-8/image-test-avif"));
+app.get("/500-1000/image-8/webp", (req, res) => res.render("500-1000/Image-8/image-test-webp"));
+app.get("/500-1000/image-8/gif", (req, res) => res.render("500-1000/Image-8/image-test-gif"));
+app.get("/500-1000/image-8/png", (req, res) => res.render("500-1000/Image-8/image-test-png"));
+app.get("/500-1000/image-8/jpg", (req, res) => res.render("500-1000/Image-8/image-test-jpg"));
+
+app.get("/500-1000/image-9/avif", (req, res) => res.render("500-1000/Image-9/image-test-avif"));
+app.get("/500-1000/image-9/webp", (req, res) => res.render("500-1000/Image-9/image-test-webp"));
+app.get("/500-1000/image-9/gif", (req, res) => res.render("500-1000/Image-9/image-test-gif"));
+app.get("/500-1000/image-9/png", (req, res) => res.render("500-1000/Image-9/image-test-png"));
+app.get("/500-1000/image-9/jpg", (req, res) => res.render("500-1000/Image-9/image-test-jpg"));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
